e5d7206f8d4c94b60b90cea940218c5dfe4f2ef6
Kishan <kishansingh88046@gmail.com>
2026-02-23 12:34:56 +0530

    otp fix 3 switched transporter to sendgrid

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,19 +1,30 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_PORT === '465',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      rejectUnauthorized: false
-    },
-    family: 4 // Force IPv4
-  });
+  // Use SendGrid if API key is available, otherwise use Gmail
+  const transporter = process.env.SENDGRID_API_KEY
+    ? nodemailer.createTransport({
+        host: 'smtp.sendgrid.net',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'apikey',
+          pass: process.env.SENDGRID_API_KEY
+        }
+      })
+    : nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT),
+        secure: process.env.EMAIL_PORT === '465',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        },
+        tls: {
+          rejectUnauthorized: false
+        },
+        family: 4
+      });
 
   const info = await transporter.sendMail({
     from: process.env.EMAIL_USER,
