169a9fdc43daef2e19b8d30b851b8598c19e6ffb
Kishan <kishansingh88046@gmail.com>
2026-02-23 12:05:46 +0530

    otp fix 2 force nodemailer for ipv4

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -11,7 +11,8 @@ const sendEmail = async ({ to, subject, html }) => {
     },
     tls: {
       rejectUnauthorized: false
-    }
+    },
+    family: 4 // Force IPv4
   });
 
   const info = await transporter.sendMail({
