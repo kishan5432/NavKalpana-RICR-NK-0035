e0fe9bdb8c5ab4f0376986267a73c0f7466943e1
Kishan <kishansingh88046@gmail.com>
2026-02-23 13:33:27 +0530

    otp fix 4 fully swithched to sendgrid

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,39 +1,15 @@
-const nodemailer = require('nodemailer');
-
 const sendEmail = async ({ to, subject, html }) => {
-  // Use SendGrid if API key is available, otherwise use Gmail
-  const transporter = process.env.SENDGRID_API_KEY
-    ? nodemailer.createTransport({
-        host: 'smtp.sendgrid.net',
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'apikey',
-          pass: process.env.SENDGRID_API_KEY
-        }
-      })
-    : nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT),
-        secure: process.env.EMAIL_PORT === '465',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        },
-        tls: {
-          rejectUnauthorized: false
-        },
-        family: 4
-      });
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const info = await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  const msg = {
     to,
+    from: process.env.EMAIL_USER,
     subject,
     html
-  });
+  };
 
-  return info;
+  await sgMail.send(msg);
 };
 
 module.exports = sendEmail;
