20421c6eb8c0f9e174a7385e3727aee9a6e2a922
Kishan <kishansingh88046@gmail.com>
2026-02-23 11:58:05 +0530

    otp not sending on production fix 1

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,10 +3,14 @@ const nodemailer = require('nodemailer');
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: parseInt(process.env.EMAIL_PORT),
+    secure: process.env.EMAIL_PORT === '465',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
